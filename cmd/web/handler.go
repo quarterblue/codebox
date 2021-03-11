b86738fa3aee067759f0299848f51de6397c815e
quarterblue <quarterslate@gmail.com>
2021-03-10 18:35:15 -0800

    Add logic for home, codebox, and create codebox routes

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -1,15 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Home path will match only exact reference to "/"
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Codebox!"))
 	return
 }
 
 func codebox(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	// Assert that id is an integer greater than 0
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Codebox code with an ID of %d", id)
 	return
 }
 
 func createCodebox(w http.ResponseWriter, r *http.Request) {
+	// Assert that request method is POST
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new codebox"))
+}
+
+func editCodebox(w http.ResponseWriter, r *http.Request) {
 	return
 }
