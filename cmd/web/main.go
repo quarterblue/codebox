d3369b4ebb3ab1f46c705e4a4d30703b68948247
quarterblue <quarterslate@gmail.com>
2021-03-11 20:25:47 -0800

    Add customized logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
+	// CLI Parser to set port
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -19,9 +24,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Server hosted on localhost %s", *addr)
+	infoLog.Printf("Server hosted on localhost %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	if err != nil {
-		panic(err)
-	}
+	errorLog.Fatal(err)
 }
